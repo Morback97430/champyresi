1281a265d2b2ebd45b8f0c80025722d48295635e
Morback974 <Morback97430@hotmail.fr>
2020-02-02 16:17:21 +0100

    30 object document

--- gestionChampignon/gestionChampignon.ino
+++ gestionChampignon/gestionChampignon.ino
@@ -1,5 +1,5 @@
 // version Arduino
-String versionArduino = "tag 1.43";
+String versionArduino = "tag 1.44";
 
 #include <ArduinoJson.h> 
 
@@ -36,7 +36,7 @@ String lireVoieSerie();
 void delayAS(unsigned long);
 
 //JSON
-const int capacity = JSON_OBJECT_SIZE(20); // capacité du JSON
+const int capacity = JSON_OBJECT_SIZE(30); // capacité du JSON
 StaticJsonDocument<capacity> document;
 
 // etalonage
